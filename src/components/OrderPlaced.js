3839e1a30ffcbde8642f45ea7f1a13df7d99e94e
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-11-26 18:58:35 +0530

    fix payment response handler

--- src/components/OrderPlaced.js
+++ src/components/OrderPlaced.js
@@ -4,10 +4,14 @@ import './../css/order-placed.css'
 
 const OrderPlaced = (props) => {
 
+    const handleClick=()=>{
+        props.disableStatus=true;
+    }
+
     return (
         <>
             {
-                props.disableStatus === false ?
+                props.disableStatus === true ?
                     <div className='order-paced-container-div'>
                         <div style={{ textAlign: 'left' }}>
                             <h6 style={{ fontSize: '15px' }}>Order has been placed successfully. <br /> Confirmation message sent!</h6>
@@ -18,6 +22,7 @@ const OrderPlaced = (props) => {
                         <div style={{ textAlign: 'left' }}>
                             <p><b>Total:</b> Rs {props.totalPrice}</p>
                         </div>
+                        <div style={{marginTop:'2rem'}}><button className="btn btn-danger btn-sm" onClick={handleClick}>Back</button></div>
                     </div>
                     : null
             }
